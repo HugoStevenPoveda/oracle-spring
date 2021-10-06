74e2a3d344be4ed7c580a5b67b8b6f8d3b98c466
hugostevencodePoveda <HugoStevenpoveda@hotmail.com>
2021-10-06 17:10:55 -0500

    primer comit para oracle

---
+++ src/main/java/com/store/costumeserver/CostumeServerApplication.java
@@ -0,0 +1,15 @@
+package com.store.costumeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostumeServerApplication.class, args);
+		System.out.println("hello word desde oracle");
+
+	}
+
+}
---
+++ src/test/java/com/store/costumeserver/CostumeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.costumeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostumeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
